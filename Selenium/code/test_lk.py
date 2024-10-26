fe81830abe8f7851cf623f36ccdc62ae981b6fa7
Fedasov <fedasov03@inbox.ru>
2024-10-26 23:22:56 +0300

    fix

---
+++ Selenium/code/test_lk.py
@@ -0,0 +1,54 @@
+import pytest
+import time
+import os
+from _pytest.fixtures import FixtureRequest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from ui.pages.base_page import BasePage
+from ui.locators import basic_locators as BC
+import page
+
+@pytest.fixture(scope='session')
+def credentials():
+    return {
+        "username": os.getenv("username"),
+        "password": os.getenv("password")
+    }
+
+@pytest.fixture(scope='session')
+def cookies(credentials, config):
+        pass
+
+class TestLK(page.BaseCase):
+    def test_lk1(self, credentials):
+        main_page = self.login_page.login(credentials['username'], credentials['password'])
+
+        main_page.click_people_icon()
+
+        main_page.search_input("Иван Карпов")
+
+        main_page.click_search_button()
+
+        main_page.click_user()
+
+        user_info = main_page.get_user_info()
+
+        assert "Учусь на СГН3-71Б 😎" in user_info["about"]
+        assert "17 октября" in user_info["birthday"]
+
+    def test_lk2(self, credentials):
+        main_page = self.login_page.login(credentials['username'], credentials['password'])
+
+        main_page.click_program_icon()
+
+        main_page.click_program()
+
+        main_page.click_lesson()
+
+        main_page.click_selected_lesson()
+
+        lesson_info = main_page.get_lesson_info()
+
+        assert "22 октября 18:00 — 21:00 Мск" in lesson_info["date"]
+        assert "Аудитория ауд.395 - зал 3 (МГТУ) и Онлайн (ссылки пока нет)" in lesson_info["location"]
--- Selenium/code/ui/locators/basic_locators.py
+++ Selenium/code/ui/locators/basic_locators.py
@@ -17,6 +17,27 @@ class MainPageLocators(BasePageLocators):
     EVENTS = (By.ID, 'events')
     READ_MORE = (By.CSS_SELECTOR, 'a.readmore')
 
+    SEARCH_PEOPLE_ICON = (By.XPATH, "//li[contains(@class, 'technopark__menu__item_159')]//a")
+    SEARCH_INPUT = (By.NAME, "q")
+    CLICK_SEARCH_BUTTON = (By.XPATH, '//input[@value="Найти"]')
+    CLICK_USER = (By.XPATH, "//p[@class='realname']/a")
+    PROFILE_ELEMENT = (By.CLASS_NAME, 'profile-about')
+    GET_USER_INFO_ABOUT = [By.XPATH, ".//div[@class='profile-about-text'][1]"]
+    GET_USER_INFO_BIRTHDAY = [By.XPATH, ".//div[contains(@class, 'profile-about-text')][2]"]
+    CLICK_PROGRAM_ICON = (By.XPATH, "//li[contains(@class, 'technopark__menu__item_160')]//a")
+    CLICK_PROGRAM = (By.XPATH,"//a[contains(@class, 'discipline-card') and contains(., '#2291: Обеспечение качества в разработке ПО')]")
+    CLICK_LESSON = (By.LINK_TEXT, 'Занятия')
+    CLICK_SELECTED_LESSON = (By.PARTIAL_LINK_TEXT, 'End-to-End тесты на Python')
+    LESSON_TITLE = (By.CLASS_NAME, "lesson-title")
+    GET_LESSON_DATE_INFO = (By.XPATH,"//div[contains(@class, 'info-pair')]/strong[text()='Дата проведения']/following-sibling::span")
+    GET_LESSON_LOCATION_INFO = (By.XPATH,"//div[contains(@class, 'info-pair')]/strong[text()='Место проведения']/following-sibling::span")
+
+class LoginPageLocators(BasePageLocators):
+    LOGIN_BUTTON = (By.XPATH, '//button[normalize-space()="вход / регистрация"]')
+    LOGIN_BUTTON_CHOOSE = (By.XPATH, '//button[normalize-space()="Продолжить с помощью почты и пароля"]')
+    INPUT_USER = (By.ID, "email")
+    INPUT_PASS = (By.ID, "password")
+    LOGIN = (By.XPATH, '//button[normalize-space()="Войти с паролем"]')
 
 class EventsPageLocators(BasePageLocators):
     pass
