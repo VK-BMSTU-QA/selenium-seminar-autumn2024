3705e5c90bf9b7c7bcc06641b3cf85b932acc887
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-10-26 12:29:31 +0300

    Code refactoring

--- Selenium/code/test_login.py
+++ Selenium/code/test_login.py
@@ -13,18 +13,18 @@ class TestLogin(BaseCase):
 class TestLK(BaseCase):
     authorize = True
 
-    # def test_user_about_info(self):
-    #     # Открываем страницу "Люди"
-    #     self.people_page = self.main_page.go_to_people_page()
-    #
-    #     # Открываем страницу пользователя с фамилией "Михалёв"
-    #     self.user_page = self.people_page.find_user("Михалёв")
-    #     about = self.user_page.get_about()
-    #     assert "Студент 4 курса ИУ5\nМладший фронтенд-разработчик в Облаке Mail" in about
+    def test_user_about_info(self):
+        # Открываем страницу "Люди"
+        self.people_page = self.main_page.go_to_people_page()
+
+        # Открываем страницу пользователя с фамилией "Михалёв"
+        self.user_page = self.people_page.find_user("Михалёв")
+        about = self.user_page.get_about()
+        assert "Студент 4 курса ИУ5\nМладший фронтенд-разработчик в Облаке Mail" in about
 
     def test_audience(self):
         # Открываем страницу занятия 22 октября
-        self.lesson_page = self.main_page.open_lesson("пн, 14 октября")
+        self.lesson_page = self.main_page.open_lesson("вт, 22 октября")
 
         # Проверяем аудиторию
         audition = self.lesson_page.get_audition()
